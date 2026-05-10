652d6ef965367dffbf067c5be3afdb4d683c5965
douglasc2001 <douglasc_2001@hotmail.com>
2026-05-10 16:53:40 -0600

    Home page commit 1

---
+++ scripts/navigation.js
@@ -0,0 +1,21 @@
+
+// Navigation menu toggle
+const navButton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+
+// Event listener for navigation toggle
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+
+// Close the menu when a link is clicked (for better mobile experience)
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Display last modified date
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
+
+
