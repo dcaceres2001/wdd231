8675d6d28d078f5be3c7dad53253246a610ff40f
douglasc2001 <douglasc_2001@hotmail.com>
2026-05-17 17:00:39 -0600

    chamber directory 1

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,92 @@
+const cards = document.querySelector('#cards');
+const gridBtn = document.querySelector('#grid-btn');
+const listBtn = document.querySelector('#list-btn');
+
+
+const displayMembers = (members) => {
+    cards.innerHTML = ""; 
+    const fragment = document.createDocumentFragment();
+
+    members.forEach(member => {
+        let card = document.createElement('section');
+        
+       
+        if (member.membership_level === 3) card.classList.add('gold-member');
+        if (member.membership_level === 2) card.classList.add('silver-member');
+
+       
+        let companyImage = document.createElement('img');
+        companyImage.src = `./images/${member.image_file_name}`;
+        companyImage.alt = member.company_name;
+        companyImage.loading = "lazy";
+        card.appendChild(companyImage);
+        companyImage.setAttribute("width", "200");
+        companyImage.setAttribute("height", "120");
+
+       
+        let infoDiv = document.createElement('div');
+        infoDiv.classList.add('card-info');
+
+        let companyName = document.createElement('h2');
+        let companyAddress = document.createElement('p');
+        let companyPhone = document.createElement('p');
+        let companyWebsite = document.createElement('a');
+        let companyType = document.createElement('p');
+        let companyMembership = document.createElement('p');
+
+        companyName.textContent = member.company_name;
+        companyAddress.textContent = member.company_addresses;
+        companyPhone.textContent = member.company_phone_number;
+        companyWebsite.textContent = member.company_website_url;
+        companyWebsite.href = member.company_website_url;
+        companyWebsite.target = "_blank";
+        companyWebsite.rel = "noopener";
+        companyType.textContent = member.business_type;
+        
+        const levels = { 1: 'Member', 2: 'Silver', 3: 'Gold' };
+        companyMembership.textContent = `Membership Level: ${levels[member.membership_level] || 'Standard'}`;
+
+       
+        infoDiv.appendChild(companyName);
+        infoDiv.appendChild(companyAddress);
+        infoDiv.appendChild(companyPhone);
+        infoDiv.appendChild(companyWebsite);
+        infoDiv.appendChild(companyType);
+        infoDiv.appendChild(companyMembership);
+
+       
+        card.appendChild(infoDiv);
+
+        fragment.appendChild(card);
+    });
+    cards.appendChild(fragment);
+}
+
+async function getMembers() {
+    try {
+        const response = await fetch('./data/members.json');
+        const data = await response.json();
+        displayMembers(data);
+
+    } catch (error) {
+        console.error('Error fetching member data:', error);
+    }
+}
+
+
+gridBtn.addEventListener('click', () => {
+    cards.className = "grid-view"; 
+    gridBtn.classList.add('active');
+    listBtn.classList.remove('active');
+});
+
+listBtn.addEventListener('click', () => {
+    cards.className = "list-view"; 
+    listBtn.classList.add('active');
+    gridBtn.classList.remove('active');
+});
+
+
+
+getMembers();
+
---
+++ scripts/prophets.js
@@ -0,0 +1,42 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+    console.table(getProphetData);
+}
+getProphetData();
+
+
+
+
+const displayProphets = (prophets) => {
+    
+    prophets.forEach(prophet => {
+        let card = document.createElement('section');
+        let fullname = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let dob = document.createElement('p');
+        let pob = document.createElement('p');
+
+        fullname.textContent = `${prophet.name} ${prophet.lastname}`;
+        dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+        pob.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullname);
+        card.appendChild(dob);
+        card.appendChild(pob);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
+
